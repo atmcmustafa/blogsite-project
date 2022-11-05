c35559929523268912e0fea9086e7a7acd697941
atmcmustafa <mstfatmcc5757@gmail.com>
2022-11-05 23:27:39 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    container: {
+      center: 'true',
+    },
+
+    fontFamily: {
+
+    },
+    colors: {
+      yellow: '#FFC017',
+      white: '#fff',
+      black: '#000',
+      lightGray: 'rgba(230, 230, 230, 1)',
+      halfGray: 'rgba(117, 117, 117, 1)',
+      twitterBlue: '#33AFFF',
+      facebookBlue: '#3346FF',
+      googleRed: '#DF1316',
+      indigo: ' #6B0CC5',
+      lightPink: '#ffd1b9',
+      lightGreen: '#34a245;',
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
